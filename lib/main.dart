f5122e5fb510fee292240495b2a523982f733315
dengchong <dengchong999@gmail.com>
2024-07-01 20:17:50 +0800

    feat: 集成screenutil 插件

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:xmshop/app/modules/tabs/views/tabs_view.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(GetMaterialApp(
-    title: "Application",
-    initialRoute: AppPages.INITIAL,
-    getPages: AppPages.routes,
-  ));
+  runApp(ScreenUtilInit(
+      designSize: const Size(1080, 2400),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: "Application",
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+        );
+      }));
 }
